93d2acc0c5eea9907e847849da5137bed9cd8181
morphOS39 <morphos39@users.noreply.github.com>
2026-06-09 16:58:19 +0200

    fix(pwa): logo links to flomily.de homepage

--- app/sw.js
+++ app/sw.js
@@ -1,4 +1,4 @@
-var CACHE = 'flomily-pwa-v8';
+var CACHE = 'flomily-pwa-v9';
 var SHELL = ['/app/', '/app/app.css', '/app/app.js', '/app/manifest.json'];
 
 self.addEventListener('install', function(e) {
